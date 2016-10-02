08cd0a7c8715014c25feb8d38464b3f1be9e5295
Henrik Pihl <H@henriks-macbook-pro.local>
2016-10-03 01:05:37 +0200

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Henrik Pihl on 2016-10-03.
+//  Copyright © 2016 Henrik Pihl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
